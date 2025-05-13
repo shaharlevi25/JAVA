6b9519c4136b400f46fbe681e0b72e720b7fec86
shaharlevi25 <shahale3@ac.sce.ac.il>
2025-05-13 15:56:00 +0300

    Java HW1

---
+++ Work 1+2/src/program/Game.java
@@ -0,0 +1,16 @@
+/**
+ * Shahar Levi 211885066
+ * Adir Yossef Mohav 207855537
+ */
+package program;
+import Gui.gui;
+
+public class Game {
+
+	public static void main(String[] args) {
+//        MainOffice game = new MainOffice(5, 4);
+//        game.play(60);
+		new gui();
+	}
+
+}
